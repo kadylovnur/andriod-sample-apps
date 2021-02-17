0c5a2d7c1061019e481d5efff648ea2d8ea329cc
sshim <mechurak@gmail.com>
2021-02-17 20:18:02 +0900

    230. Signing In The User

--- 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/activities/SignInActivity.kt
+++ 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/activities/SignInActivity.kt
@@ -1,18 +1,27 @@
 package com.shimnssso.android.projemanag.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.shimnssso.android.projemanag.R
 import com.shimnssso.android.projemanag.databinding.ActivitySignInBinding
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
     private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         // This is used to hide the status bar and make the splash screen as a full screen activity.
         window.setFlags(
@@ -21,6 +30,10 @@ class SignInActivity : AppCompatActivity() {
         )
 
         setupActionBar()
+
+        binding.btnSignIn.setOnClickListener {
+            signInRegisteredUser()
+        }
     }
 
     /**
@@ -39,4 +52,54 @@ class SignInActivity : AppCompatActivity() {
         binding.toolbarSignInActivity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+        val password: String = binding.etPassword.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    /**
+     * A function to validate the entries of a user.
+     */
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
+    }
 }
