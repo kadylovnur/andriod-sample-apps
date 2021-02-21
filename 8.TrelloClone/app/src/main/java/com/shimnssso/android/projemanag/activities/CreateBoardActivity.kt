8cac926573fafab3e5d0aa866b5d3acc0ca20c73
sshim <mechurak@gmail.com>
2021-02-21 17:48:06 +0900

    248. Creating A Board

--- 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/activities/CreateBoardActivity.kt
+++ 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/activities/CreateBoardActivity.kt
@@ -6,21 +6,31 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.shimnssso.android.projemanag.R
 import com.shimnssso.android.projemanag.databinding.ActivityCreateBoardBinding
+import com.shimnssso.android.projemanag.firebase.FirestoreClass
+import com.shimnssso.android.projemanag.models.Board
 import com.shimnssso.android.projemanag.utils.Constants
 import java.io.IOException
 
 class CreateBoardActivity : BaseActivity() {
     private lateinit var binding: ActivityCreateBoardBinding
 
+    // Add a global variable for URI of a selected image from phone storage.
     private var mSelectedImageFileUri: Uri? = null
 
+    // A global variable for Username
     private lateinit var mUserName: String
+
+    // A global variable for a board image URL
+    private var mBoardImageURL: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +58,21 @@ class CreateBoardActivity : BaseActivity() {
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                     Constants.READ_STORAGE_PERMISSION_CODE
                 )
+            }
+        }
+
+        binding.btnCreate.setOnClickListener {
+
+            // Here if the image is not selected then update the other details of user.
+            if (mSelectedImageFileUri != null) {
+
+                uploadBoardImage()
+            } else {
+
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                // Call a function to update create a board.
+                createBoard()
             }
         }
     }
@@ -117,6 +142,73 @@ class CreateBoardActivity : BaseActivity() {
 
         binding.toolbarCreateBoardActivity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    /**
+     * A function to upload the Board Image to storage and getting the downloadable URL of the image.
+     */
+    private fun uploadBoardImage() {
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        //getting the storage reference
+        val sRef: StorageReference = FirebaseStorage.getInstance().reference.child(
+            "BOARD_IMAGE" + System.currentTimeMillis() + "."
+                    + Constants.getFileExtension(this@CreateBoardActivity, mSelectedImageFileUri)
+        )
+
+        //adding the file to reference
+        sRef.putFile(mSelectedImageFileUri!!)
+            .addOnSuccessListener { taskSnapshot ->
+                // The image upload is success
+                Log.e(
+                    "Firebase Image URL",
+                    taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+                )
+
+                // Get the downloadable url from the task snapshot
+                taskSnapshot.metadata!!.reference!!.downloadUrl
+                    .addOnSuccessListener { uri ->
+                        Log.e("Downloadable Image URL", uri.toString())
+
+                        // assign the image url to the variable.
+                        mBoardImageURL = uri.toString()
+
+                        // Call a function to create the board.
+                        createBoard()
+                    }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(
+                    this@CreateBoardActivity,
+                    exception.message,
+                    Toast.LENGTH_LONG
+                ).show()
+
+                hideProgressDialog()
+            }
+    }
+
+
+    /**
+     * A function to make an entry of a board in the database.
+     */
+    private fun createBoard() {
+
+        //  A list is created to add the assigned members.
+        //  This can be modified later on as of now the user itself will be the member of the board.
+        val assignedUsersArrayList: ArrayList<String> = ArrayList()
+        assignedUsersArrayList.add(getCurrentUserID()) // adding the current user id.
+
+        // Creating the instance of the Board and adding the values as per parameters.
+        val board = Board(
+            binding.etBoardName.text.toString(),
+            mBoardImageURL,
+            mUserName,
+            assignedUsersArrayList
+        )
+
+        FirestoreClass().createBoard(this@CreateBoardActivity, board)
+    }
+
 
     /**
      * A function for notifying the board is created successfully.
