f4c5a8b03a5adea8673cf63c478e7bbb75298225
sshim <mechurak@gmail.com>
2021-02-21 18:44:09 +0900

    249. Preparing The Boards Recyclerview Adapter And UI Elements

---
+++ 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/BoardItemsAdapter.kt
@@ -0,0 +1,93 @@
+package com.shimnssso.android.projemanag
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.shimnssso.android.projemanag.databinding.ItemBoardBinding
+import com.shimnssso.android.projemanag.models.Board
+
+open class BoardItemsAdapter(
+    private val context: Context,
+    private var list: ArrayList<Board>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var onClickListener: OnClickListener? = null
+
+    /**
+     * Inflates the item views which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemBinding =
+            ItemBoardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(itemBinding)
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder) {
+            holder.bind(context, model)
+
+            holder.itemView.setOnClickListener {
+
+                if (onClickListener != null) {
+                    onClickListener!!.onClick(position, model)
+                }
+            }
+        }
+    }
+
+    /**
+     * Gets the number of items in the list
+     */
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * A function for OnClickListener where the Interface is the expected parameter..
+     */
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
+    }
+
+    /**
+     * An interface for onclick items.
+     */
+    interface OnClickListener {
+        fun onClick(position: Int, model: Board)
+    }
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+    private class MyViewHolder(private val itemBinding: ItemBoardBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(context: Context, model: Board) {
+            Glide
+                .with(context)
+                .load(model.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_board_place_holder)
+                .into(itemBinding.ivBoardImage)
+
+            itemBinding.tvName.text = model.name
+            itemBinding.tvCreatedBy.text = "Created By : ${model.createdBy}"
+        }
+    }
+}
