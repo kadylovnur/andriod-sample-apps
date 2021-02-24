7fe2acf13a3b6121b6f2f0c15959a0dc9e4ccff7
sshim <mechurak@gmail.com>
2021-02-24 20:31:37 +0900

    267. Loading Card Details To Set The Title Of The Card

--- 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/activities/CardDetailsActivity.kt
+++ 8.TrelloClone/app/src/main/java/com/shimnssso/android/projemanag/activities/CardDetailsActivity.kt
@@ -2,14 +2,62 @@ package com.shimnssso.android.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shimnssso.android.projemanag.R
 import com.shimnssso.android.projemanag.databinding.ActivityCardDetailsBinding
+import com.shimnssso.android.projemanag.models.Board
+import com.shimnssso.android.projemanag.utils.Constants
 
 class CardDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCardDetailsBinding
+
+    // A global variable for board details
+    private lateinit var mBoardDetails: Board
+
+    // A global variable for task item position
+    private var mTaskListPosition: Int = -1
+
+    // A global variable for card item position
+    private var mCardPosition: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCardDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getIntentData()
+
+        setupActionBar()
+    }
+
+    /**
+     * A function to setup action bar
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarCardDetailsActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
+        }
+
+        binding.toolbarCardDetailsActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+    // END
+
+    // A function to get all the data that is sent through intent.
+    private fun getIntentData() {
+
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
