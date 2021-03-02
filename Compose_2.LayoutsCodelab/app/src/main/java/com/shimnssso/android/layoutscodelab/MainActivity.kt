1b61eb400c65c44c6d090d36eb8a769d8d85bcc1
sshim <mechurak@gmail.com>
2021-03-03 08:49:14 +0900

    Modifiers (Layouts in Jetpack Compose)
    https://developer.android.com/codelabs/jetpack-compose-layouts#2

--- Compose_2.LayoutsCodelab/app/src/main/java/com/shimnssso/android/layoutscodelab/MainActivity.kt
+++ Compose_2.LayoutsCodelab/app/src/main/java/com/shimnssso/android/layoutscodelab/MainActivity.kt
@@ -3,36 +3,73 @@ package com.shimnssso.android.layoutscodelab
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shimnssso.android.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LayoutsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            PhotographerCardView()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)  // outer
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {
+                // Ignore
+            })
+            .padding(16.dp)  // order is matter
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PhotographerCardView() {
     LayoutsCodelabTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
