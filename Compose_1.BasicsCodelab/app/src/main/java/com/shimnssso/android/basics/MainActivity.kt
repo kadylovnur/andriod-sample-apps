cd1733c8542a7ffa7b7fc46d5336464afef6b83d
sshim <mechurak@gmail.com>
2021-03-03 00:21:05 +0900

    Add BasicsCodelab project (Jetpack Compose basics)
    https://developer.android.com/codelabs/jetpack-compose-basics#0

---
+++ Compose_1.BasicsCodelab/app/src/main/java/com/shimnssso/android/basics/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.shimnssso.android.basics
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shimnssso.android.basics.ui.theme.BasicsCodelabTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    BasicsCodelabTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Preview("MyScreen preview")
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MyScreenContent()
+    }
+}
